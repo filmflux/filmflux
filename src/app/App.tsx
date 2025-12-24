d693745d0c9b4716e79b959809e0ebb5e1abe5d6
Calvin Chiramal <calvin.wilson@ultrahuman.com>
2025-12-24 09:55:52 +0530

    Installs tailwind

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-
-function App() {
+const App = () => {
   return (
- <div>
-    <h1>FilmFlux</h1>
- </div>
-  )
-}
+    <div>
+      <h1>FilmFlux</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
